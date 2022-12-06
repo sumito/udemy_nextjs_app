c96c53e3202675a6a8215dc515f13f5a38321961
sumito <sumito.temma@gmail.com>
2022-12-07 03:24:42 +0900

    blogpage commit

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,10 +1,26 @@
-import Layout from '../components/Layout'
-const Blog = () => {
+import Layout from '../components/Layout';
+import Post from "../components/Post";
+import { getAllPostsData } from '../lib/posts';
+
+const Blog  = ({ posts }) => {
     return (
-        <Layout title="Home">
-        <p className="text-4xl">Blog page</p>
-  
+        <Layout title="Blog">
+        
+        {" : "}
+          <ul className="m-10">
+            {posts && posts.map( (post) => <Post key={post.id} post={post} /> )}
+          </ul>
       </Layout>
-    )
+    );
+};
+
+export default Blog;
+
+export async function getStaticProps(){
+  const posts = await getAllPostsData();
+  return {
+    props : {posts},
+  };
 }
-export default Blog
+
+
